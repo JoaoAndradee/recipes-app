2713e22e54f4fba6f889aab14f3c37e6c2c3ef45
joao <joao.dev94@gmail.com>
2023-10-03 17:03:09 -0300

    Adição de css em App, Foot e Profile

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import mealIcon from '../images/mealIcon.svg';
-import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/icone-prato.svg';
+import drinkIcon from '../images/icone-bebida.svg';
 import '../styles/Footer.css';
+import styles from './Footer.module.css';
 
 function Footer() {
   const history = useHistory();
   return (
     <footer
       data-testid="footer"
+      id={ styles.footerContainer }
     >
       <input
         type="image"
         src={ drinkIcon }
+        className={ styles.footerIcons }
         name="drinkIcon"
         alt="drinkIcon"
         data-testid="drinks-bottom-btn"
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import profileIcon from '../images/profileIcon.svg';
 import Footer from './Footer';
+import Perfil from '../images/Perfil.svg';
+import Logout from '../images/Logout.svg';
+import Done from '../images/done.svg';
+import Favorite from '../images/favorite.svg';
+import styles from './Profile.module.css';
 
 function Profile() {
   const handleClick = () => {
@@ -20,35 +25,50 @@ function Profile() {
     <div>
       <Header
         dataTestIdProfile="profile-top-btn"
-        title="Profile"
         profileIcon={ profileIcon }
       />
-      <p data-testid="profile-email">{emailLocalStorage}</p>
-      <Link to="/done-recipes">
-        <button
-          type="button"
-          data-testid="profile-done-btn"
-        >
-          Done Recipes
-        </button>
-      </Link>
-      <Link to="/favorite-recipes">
-        <button
-          type="button"
-          data-testid="profile-favorite-btn"
-        >
-          Favorite Recipes
-        </button>
-      </Link>
-      <Link to="/">
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ handleClick }
-        >
-          Logout
-        </button>
-      </Link>
+      <div className={ styles.container }>
+        <img
+          src={ Perfil }
+          alt="Profile"
+          width="50"
+          height="50"
+          className={ styles.profileIcon }
+        />
+        <h3 className={ styles.profileText }>PROFILE</h3>
+        <p data-testid="profile-email">{emailLocalStorage}</p>
+        <Link to="/done-recipes">
+          <button
+            type="button"
+            className={ styles.buttonProfile }
+            data-testid="profile-done-btn"
+          >
+            <img className={ styles.imgsTeste } src={ Done } alt="Done recipes" />
+            Done Recipes
+          </button>
+        </Link>
+        <Link to="/favorite-recipes">
+          <button
+            className={ styles.buttonProfile }
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            <img className={ styles.imgsTeste } src={ Favorite } alt="Done recipes" />
+            Favorite Recipes
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            className={ styles.buttonProfile }
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ handleClick }
+          >
+            <img className={ styles.imgsTeste } src={ Logout } alt="Done recipes" />
+            Logout
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
